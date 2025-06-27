18b437364931635ccc1cae34c83922a5975cace9
drb101005 <drb101005@gmail.com>
2025-06-27 18:37:06 +0530

    Update 1672_richest_customer_wealth.py

--- 1672_richest_customer_wealth.py
+++ 1672_richest_customer_wealth.py
@@ -1,6 +1,17 @@
 '''
 1 2 3
 3 2 1
+7 8 1
+
+            bank1   bank2   bank3
+customer1       1       2       3
+customer2       3       2       1
+customer3       7       8       1
+
+output: 1+2+3
+        3+2+1
+        7+8+1
+        Customer 3 is the richest
 
 1 5
 7 3
@@ -16,8 +27,10 @@ class Solution(object):
         :rtype: int
         """
         max_wealth = 0
-        for customer in accounts:
-            wealth = sum(customer)
+#The term amount directly adds the lines in y asix
+        for amount in accounts:
+            wealth = sum(amount)
             if wealth > max_wealth:
                 max_wealth = wealth
         return max_wealth
+ 
