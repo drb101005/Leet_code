760cdbff37a5a2fb3f45d159e9f98d990b82650c
drb101005 <drb101005@gmail.com>
2025-09-26 15:48:15 +0530

    Simplify anagram check logic in Solution class
    Removed unnecessary HashMap usage and replaced with direct string comparison after sorting character arrays. This improves readability and efficiency of the isAnagram method.

--- 242_Valid_Anagram.java
+++ 242_Valid_Anagram.java
@@ -17,7 +17,42 @@
 
 // 1 <= s.length, t.length <= 5 * 104
 
-import java.util.HashMap;
+// import java.util.HashMap;
+// import java.util.Arrays;
+
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         char[] arr1 = s.toCharArray();
+//         char[] arr2 = t.toCharArray();
+//         Arrays.sort(arr1);
+//         Arrays.sort(arr2);
+//         s = new String(arr1);
+//         t = new String(arr2);
+
+//         HashMap<Integer, String> hm1 = new HashMap<>();
+//         HashMap<Integer, String> hm2 = new HashMap<>();
+
+//         if (s.length() == t.length()) {
+//             int j = 0, k = 0, h = 0, g = 0;
+//             while (j < s.length() && h < t.length()) {
+//                 hm1.put(j, String.valueOf(s.charAt(k)));
+//                 j = j + 1;
+//                 k = k + 1;
+//                 hm2.put(h, String.valueOf(t.charAt(g)));
+//                 h = h + 1;
+//                 g = g + 1;
+//             }
+//             if (hm1.equals(hm2)){
+//                 return true;
+//             }
+//             return false;
+//         } else {
+//             return false;
+//         }
+//     }
+// }
+
+// Better Approch : 
 import java.util.Arrays;
 
 class Solution {
@@ -28,25 +63,10 @@ class Solution {
         Arrays.sort(arr2);
         s = new String(arr1);
         t = new String(arr2);
-
-        HashMap<Integer, String> hm1 = new HashMap<>();
-        HashMap<Integer, String> hm2 = new HashMap<>();
-
-        if (s.length() == t.length()) {
-            int j = 0, k = 0, h = 0, g = 0;
-            while (j < s.length() && h < t.length()) {
-                hm1.put(j, String.valueOf(s.charAt(k)));
-                j = j + 1;
-                k = k + 1;
-                hm2.put(h, String.valueOf(t.charAt(g)));
-                h = h + 1;
-                g = g + 1;
-            }
-            if (hm1.equals(hm2)){
-                return true;
-            }
-            return false;
-        } else {
+        if (s.equals(t)){
+            return true;
+        }
+        else{
             return false;
         }
     }
