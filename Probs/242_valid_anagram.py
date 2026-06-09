d34df6629ee65ba102b5424ed53da3b6199fb159
drb101005 <drb101005@gmail.com>
2026-06-09 12:00:00 +0530

    Probs/242_valid_anagram.py

--- Probs/242_valid_anagram.py
+++ Probs/242_valid_anagram.py
@@ -1,25 +1,23 @@
 # Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 
- 
 
 # Example 1:
 
 # Input: s = "anagram", t = "nagaram"
-
 # Output: true
+
 
 # Example 2:
 
 # Input: s = "rat", t = "car"
-
 # Output: false
 
- 
 
 # Constraints:
 
-# 1 <= s.length, t.length <= 5 * 104
+# 1 <= s.length, t.length <= 5 * 10^4
 # s and t consist of lowercase English letters.
+
 
 class Solution(object):
     def isAnagram(self, s, t):
@@ -28,32 +26,17 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        i = 0
-        s = sorted(s)
-        t = sorted(t)
-        if(len(s) == len(t)):
-            while(i < len(s)):
-                if(s[i] == t[i]):
-                    i = i + 1 
-                else:
-                    return False
-            return True
-        else:
-            return False
-# Best Solution
-class Solution(object):
-    def isAnagram(self, s, t):
         if len(s) != len(t):
             return False
 
-        count = [0] * 26  
+        count = [0] * 26
 
         for i in range(len(s)):
             count[ord(s[i]) - ord('a')] += 1
             count[ord(t[i]) - ord('a')] -= 1
 
-        for c in count:
-            if c != 0:
+        for value in count:
+            if value != 0:
                 return False
 
         return True
