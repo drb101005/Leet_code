65228b5d0287caac80e88324055be5a42a086857
drb101005 <drb101005@gmail.com>
2026-06-09 12:00:00 +0530

    Probs/169_Majority_Element.py

---
+++ Probs/169_Majority_Element.py
@@ -0,0 +1,41 @@
+# Given an array nums of size n, return the majority element.
+# The majority element is the element that appears more than n / 2 times.
+
+
+# Example 1:
+
+# Input: nums = [3,2,3]
+# Output: 3
+
+
+# Example 2:
+
+# Input: nums = [2,2,1,1,1,2,2]
+# Output: 2
+
+
+# Constraints:
+
+# n == nums.length
+# 1 <= nums.length <= 5 * 10^4
+# -10^9 <= nums[i] <= 10^9
+
+
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count = 0
+        candidate = None
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+
+        return candidate
